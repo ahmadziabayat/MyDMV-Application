62ba4c1feffd89d5dd6db31e3ff80bc31822bcb9
Ahmad Zia Bayat <ahmadzia_10@hotmail.com>
2018-04-06 23:28:29 -0400

    refactoring files and folders adding package.json and gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    webserver = require('gulp-webserver');
+
+gulp.task('js', function() {
+    gulp.src('builds/dmv_js/**/*');
+});
+
+gulp.task('html', function() {
+    gulp.src('builds/*.html');
+});
+
+gulp.task('css', function() {
+    gulp.src('builds/dmv_css/*.css');
+});
+
+gulp.task('watch', function() {
+    gulp.watch('builds/dmv_js/**/*', ['js']);
+    gulp.watch('builds/dmv_css/*.css', ['css']);
+    gulp.watch(['builds/*.html'], ['html']);
+});
+
+gulp.task('webserver', function() {
+    gulp.src('builds/')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
+});
+
+gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
